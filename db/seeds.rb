e9c34a07661fd1292854698b252616445e10847d
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-06-18 23:14:52 +0100

    got the read part of CRUD done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(
+  title: "Shopping",
+  detail: "Do the weeks knick knack purchases and ensure that you have enough bog roll as well"
+  )
+
+Task.create(
+  title: "Ironing",
+  detail: "Get rid of that pile of junk you've got in the washing room"
+  )
+
+Task.create(
+  title: "Cats Tray",
+  detail: "Clean out the litter tray before SHE comes back home"
+  )
+  
+
+
